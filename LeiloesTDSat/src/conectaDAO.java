1cc2ca48fb060223d988bfe876bcbde1edc6f823
Guilherme <guilhermepvargas@hotmail.com>
2026-03-31 21:48:59 -0300

    alterações realizadas
    Implementa cadastro de produtos no banco;
    Adiciona mensagem de sucesso ao cadastrar produto;
    Implementa listagem de produtos do banco;
    
    Correção de erro com o banco de dados;

--- LeiloesTDSat/src/conectaDAO.java
+++ LeiloesTDSat/src/conectaDAO.java
@@ -22,12 +22,15 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/uc11?useSSL=false&serverTimezone=UTC",
+                "root",
+                "3a9xwtjA!"
+            );
             
         } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+            javax.swing.JOptionPane.showMessageDialog(null, "Erro ConectaDAO: " + erro.getMessage());
         }
         return conn;
     }
-    
 }
